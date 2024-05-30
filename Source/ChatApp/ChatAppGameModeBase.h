702033fafa7e9a5f8f26dfa1bf2401b8f8ef1157
Dmitry Koba <55078980+Tvix57@users.noreply.github.com>
2024-05-30 22:46:14 +0700

    init

---
+++ Source/ChatApp/ChatAppGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChatAppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHATAPP_API AChatAppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
